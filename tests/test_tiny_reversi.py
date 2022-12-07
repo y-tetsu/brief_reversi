6289388077895c20b2adef6b34287dc87544d89f
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2022-12-07 18:08:30 +0900

    add tests

---
+++ tests/test_tiny_reversi.py
@@ -0,0 +1,7 @@
+import unittest
+from tiny_reversi import reversi
+
+class TestTinyRevers(unittest.TestCase):
+    def test_com_vs_com(self):
+        expected = [0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 1, 1, 1, 1, 1, 1, 1, 2, 3, 1, 1, 1, 1, 1, 1, 1, 2, 3, 1, 1, 1, 1, 1, 1, 1, 2, 3, 1, 2, 1, 2, 1, 1, 1, 2, 3, 1, 2, 1, 1, 2, 1, 1, 2, 3, 1, 1, 2, 1, 1, 2, 1, 2, 3, 1, 1, 1, 2, 2, 1, 2, 2, 3, 2, 2, 2, 2, 2, 2, 2, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.assertEqual(reversi(True), expected)
