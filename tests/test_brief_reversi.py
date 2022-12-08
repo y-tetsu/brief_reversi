90263da81089f23e4469a5b12bd520cd1c98ce31
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2022-12-09 08:44:12 +0900

    rename module

--- tests/test_brief_reversi.py
+++ tests/test_brief_reversi.py
@@ -1,8 +1,8 @@
 import unittest
-from tiny_reversi import reversi
+from brief_reversi import reversi
 
 
-class TestTinyRevers(unittest.TestCase):
+class TestBriefRevers(unittest.TestCase):
     def test_com_vs_com(self):
         expected = [
             0, 0, 0, 0, 0, 0, 0, 0, 0,
